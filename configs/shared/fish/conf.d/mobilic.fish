6fce0a8a421a0acfdfb6c5c624d601f0846a0b7c
Gaspard Lonchampt <lonchamptgaspard@gmail.com>
2026-01-29 10:39:57 +0100

    fix(fish): remove -i flag from Scalingo db-tunnel aliases to use ssh-agent

--- configs/shared/fish/conf.d/mobilic.fish
+++ configs/shared/fish/conf.d/mobilic.fish
@@ -1,5 +1,6 @@
 # Aliases Mobilic - tunnels et connexions DB Scalingo
-alias prod_dbtunnel 'scalingo --app mobilic-api db-tunnel -i ~/.ssh/id_ed25519_mobilic_scalingo_2025 SCALINGO_POSTGRESQL_URL'
-alias staging_dbtunnel 'scalingo --app mobilic-api-staging db-tunnel -i ~/.ssh/id_ed25519_mobilic_scalingo_2025 SCALINGO_POSTGRESQL_URL'
-alias sandbox_dbtunnel 'scalingo --app mobilic-api-sandbox db-tunnel -i ~/.ssh/id_ed25519_mobilic_scalingo_2025 SCALINGO_POSTGRESQL_URL'
+# Note: pas de -i, utilise l'agent SSH (keychain charge la clé au démarrage)
+alias prod_dbtunnel 'scalingo --app mobilic-api db-tunnel SCALINGO_POSTGRESQL_URL'
+alias staging_dbtunnel 'scalingo --app mobilic-api-staging db-tunnel SCALINGO_POSTGRESQL_URL'
+alias sandbox_dbtunnel 'scalingo --app mobilic-api-sandbox db-tunnel SCALINGO_POSTGRESQL_URL'
 alias prod_db 'scalingo --app mobilic-api pgsql-console'
